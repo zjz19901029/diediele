a423b59fd226a69cbc6c9504c3843b8927d30858
周静之 <mirror.zhou@platenogroup.com>
2018-01-19 19:01:33 +0800

    no message

--- game.js
+++ game.js
@@ -1,3 +1,13 @@
 import './js/libs/weapp-adapter'
 import init from './js/main'
+import createjs from './js/libs/preloadjs.min.js'
+
+let queue = new createjs.LoadQueue()
+queue.on("complete", init, this)
+// queue.loadFile({id:"sound", src:"http://path/to/sound.mp3"})
+queue.loadManifest([
+    'images/triangle.png',
+    'images/cube.jpg'
+])
+
 init()
